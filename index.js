a81f31e72f65a9d9540f0e5310c3cfdfcde87fb3
Durounseki <156300780+Durounseki@users.noreply.github.com>
2024-09-14 02:20:56 +0000

    feat: post exercise

--- index.js
+++ index.js
@@ -58,11 +58,47 @@ app.post("/api/users", (req, res) => {
       res.json({ error: err });
     });
 });
-
+//Get all users
 app.get("/api/users", (req, res) => {
   User.find({})
     .then((data) => {
       res.json(data);
+    })
+    .catch((err) => {
+      res.json({ error: err });
+    });
+});
+//Post exercise
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const id = req.params._id;
+  let { description, duration, date } = req.body;
+  User.findById(id)
+    .then((data) => {
+      const username = data.username;
+      //Check if there is a date
+      if (date === "" || date === undefined) {
+        date = new Date().toDateString();
+      }
+      const newExercise = new Exercise({
+        username: username,
+        description: description,
+        duration: duration,
+        date: date,
+      });
+      newExercise
+        .save()
+        .then((data) => {
+          res.json({
+            _id: id,
+            username: username,
+            date: data.date,
+            duration: data.duration,
+            description: data.description,
+          });
+        })
+        .catch((err) => {
+          res.json({ error: err });
+        });
     })
     .catch((err) => {
       res.json({ error: err });
